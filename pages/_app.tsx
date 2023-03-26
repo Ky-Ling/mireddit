7dd11d0d620919955d8bfcc55fbab618f5bec362
Ky-Ling <2845740149@qq.com>
2023-03-26 18:44:31 +0800

    feat: setup for apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { ApolloProvider } from '@apollo/client';
 import Header from '@/components/Header';
+import client from '../apollo-client';
 
 export default function App({
 	Component,
 	pageProps: { session, ...pageProps },
 }: AppProps) {
 	return (
-		<SessionProvider session={session}>
-			<div className="h-screen overflow-y-scroll bg-slate-200">
-				<Header />
-				<Component {...pageProps} />
-			</div>
-		</SessionProvider>
+		<ApolloProvider client={client}>
+			<SessionProvider session={session}>
+				<div className="h-screen overflow-y-scroll bg-slate-200">
+					<Header />
+					<Component {...pageProps} />
+				</div>
+			</SessionProvider>
+		</ApolloProvider>
 	);
 }
