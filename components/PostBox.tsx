e52ad9f23d8a51456a19fab5cafcae1b73575546
Ky-Ling <2845740149@qq.com>
2023-03-27 20:04:40 +0800

    feat: build the PostBox component with react-hook-form

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { useSession } from 'next-auth/react';
+import { useForm } from 'react-hook-form';
+import { AiOutlineLink } from 'react-icons/ai';
+import { HiPhotograph } from 'react-icons/hi';
 import Avatar from './Avatar';
+
+type FormData = {
+	postTitle: string;
+	postBody: string;
+	postImage: string;
+	subreddit: string;
+};
 
 const PostBox = () => {
 	// const { data: session  } = useSession();
 	const session = true;
 
+	const {
+		register,
+		setValue,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<FormData>();
+
 	return (
-		<form>
+		<form className="sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2">
 			<div className="flex items-center space-x-3">
 				<Avatar />
 				<input
+					{...register('postTitle', { required: true })}
 					className="bg-gray-50 p-2 pl-5 outline-none rounded-md flex-1"
 					type="text"
 					placeholder={
@@ -18,7 +37,24 @@ const PostBox = () => {
 					}
 					disabled={!session}
 				/>
+
+				<HiPhotograph className={`h-6 text-gray-300 cursor-pointer`} />
+				<AiOutlineLink className="h-6 text-gray-300" />
 			</div>
+
+			{!!watch('postTitle') && (
+				<div>
+					<div>
+						<p>Body</p>
+						<input
+							className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+							{...register('postBody')}
+							type="text"
+							placeholder="Text (optional)"
+						/>
+					</div>
+				</div>
+			)}
 		</form>
 	);
 };
