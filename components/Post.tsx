fbf93c2aa526bc0bc8218cc243952b183fabcda9
Ky-Ling <2845740149@qq.com>
2023-03-29 20:25:10 +0800

    feat: build the header section of Post component

--- components/Post.tsx
+++ components/Post.tsx
@@ -9,6 +9,9 @@ import {
 import { BsBookmarkCheck } from 'react-icons/bs';
 import { HiOutlineChatAlt2 } from 'react-icons/hi';
 import { RxDotsHorizontal } from 'react-icons/rx';
+import TimAgo from 'react-timeago';
+
+import Avatar from './Avatar';
 
 interface PostProps {
 	post: Post;
@@ -16,14 +19,22 @@ interface PostProps {
 
 const Post: React.FC<PostProps> = ({ post }) => {
 	return (
-		<div>
+		<div className="flex cursor-pointer rounded-md border border-gray-300 bg-white shadow-sm hover:border hover:border-gray-600">
 			<div className="flex flex-col items-center justify-start space-y-1 rounded-l-md bg-gray-50 p-4 text-gray-400">
 				<AiOutlineArrowUp className="voteButtons hover:text-red-400" />
 				<p className="text-black font-bold text-xs">0</p>
 				<AiOutlineArrowDown className="voteButtons hover:text-blue-400" />
 			</div>
-			<div>
-				<div></div>
+			<div className="p-3 pb-1">
+				<div className="flex items-center space-x-2">
+					<Avatar seed={post.subreddit[0]?.topic} />
+					<p className="text-xs text-gray-400">
+						<span className="font-bold text-black hover:text-blue-400 hover:underline">
+							r/{post.subreddit[0]?.topic}
+						</span>{' '}
+						&bull; Posted by u/{post.username} <TimAgo date={post.created_at} />
+					</p>
+				</div>
 			</div>
 		</div>
 	);
